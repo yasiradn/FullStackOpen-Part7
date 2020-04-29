87aa0df1d43ff6b26d706f89fdeffa249d6eb7e4
yasir <aff3ad@gmail.com>
2020-04-29 15:14:13 +0300

    7.14

--- bloglist-redux/src/components/User.js
+++ bloglist-redux/src/components/User.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import {
+    useParams, BrowserRouter as Router,
+    Switch, Route, Link
+  } from "react-router-dom"
 import {fetchUsers} from '../reducers/userReducers'
 import { useDispatch,useSelector } from 'react-redux'
+import user from '../services/user'
 const User = () => {
     const disptach = useDispatch()
     disptach(fetchUsers())
     const getUsers = useSelector(state=>state.users)
-    console.log(getUsers)
+    if (!getUsers) {
+        return null
+      }
     const ShowUserAndBlogs = () => {
         return(
             <div>
+                <h3>Users</h3>
                 <table>
                     <thead>
                         <th>blogs created</th>
@@ -16,8 +24,8 @@ const User = () => {
                         {getUsers.map(user => 
                             <div key={user.id}>
                                 <tbody> 
-                                    <td>{user.name}</td> 
-                                    <td>{user.blogs.length}</td> 
+                                    <Link to={`/users/${user.id}`}>{user.name}</Link>
+                                    <td>{user.blogs.length}</td>
                                 </tbody>
                             </div>
                         )} 
@@ -25,11 +33,33 @@ const User = () => {
             </div>
         )
     }
+    const UserInfo = () => {
+           console.log('username')
+            const id = useParams().id
+            const user = getUsers.find(item => item.id === id)
+            console.log('username', user)
+            if(!user){
+                return null
+            }
+            return (
+              <div>
+               <h2>{user.name}</h2>
+               <h3>Added Blogs</h3>
+               {user.blogs.map(blog=><div>
+                   <li>{blog.title}</li>
+               </div>)}
+              </div>
+            )
+    }
     return(
         <div>
-            <h3>Users</h3>
             <br/>
-            <ShowUserAndBlogs/>
+            <Router>
+                <Switch>
+                <Route path ="/users/:id"><UserInfo/></Route>
+                <Route path ="/"><ShowUserAndBlogs/></Route>
+                </Switch>
+            </Router>
         </div>
     )
 }
